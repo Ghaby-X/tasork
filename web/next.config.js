1cf8ffde063d063d46eced7304eb9695419c3081
Ghaby-X <gabrielanyaele21@gmail.com>
2025-05-29 08:45:37 +0000

    modify config to support next exports

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    reactStrictMode: false,
+  output: 'export',
+  reactStrictMode: false,
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 }
 
 module.exports = nextConfig
